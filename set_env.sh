5e7c215226e904dbc67d1fad2344da481732696f
zyzornatti <tobimustapha53@gmail.com>
2022-11-20 13:43:47 +0100

    done

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+setx POSTGRES_USERNAME=zyzornatti
+setx POSTGRES_PASSWORD=password1
+setx POSTGRES_HOST=postgres.cx0cmhhvv9km.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB=postgres
+setx AWS_BUCKET=myp3newbk
+setx AWS_REGION=us-east-1
+setx AWS_PROFILE=default
+setx JWT_SECRET=testing
+setx URL=http://localhost:8100
