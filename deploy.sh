29fd14f8a96224299cb3943dcd1bcecfb60ea24e
zyzornatti <tobimustapha53@gmail.com>
2022-11-21 16:45:45 +0100

    done

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-kubectl delete deploy backend-user
-kubectl delete deploy backend-feed
-kubectl delete deploy reverseproxy
-kubectl delete deploy frontend
+kubectl apply -f user-deployment.yaml
+kubectl apply -f feed-deployment.yaml
+kubectl apply -f reverse-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
+
+kubectl apply -f user-service.yaml
+kubectl apply -f feed-service.yaml
+kubectl apply -f reverse-service.yaml
+kubectl apply -f frontend-service.yaml
